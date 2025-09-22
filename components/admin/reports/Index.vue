c3b674816bb58acc8a7d75c7210af2062f9444c7
Marcela Rios <marcelariosvargas@gmail.com>
2025-09-21 23:36:25 -0400

    reporte consulta de transferencia

--- components/admin/reports/Index.vue
+++ components/admin/reports/Index.vue
@@ -42,9 +42,11 @@
                     <XInputText name="codigo" label="Numero de cuenta" label-required placeholder="Ingresa numero" />
                 </div>
                 <!-- Tarjeta 2 -->
-                <div
-                    class="w-full sm:w-1/3 p-4 bg-white border border-gray-200 rounded-2xl h-65 flex flex-col justify-between">
-                    <h3 class="text-black font-bold text-[16px] m-0">Detalles de la transferencia</h3>
+                <div class="w-full sm:w-1/3 p-4 bg-white border border-gray-200 rounded-2xl h-auto flex flex-col">
+                    <h3 class="text-black font-bold text-[16px] m-0">
+                        Detalles de la transferencia
+                    </h3>
+
                     <XDatePicker v-model="date" label="Fecha" name="fechaTransferencia" label-required
                         :rules="[(v: any) => !!v || 'El campo es requerido']" :pt="{
                             calendarContainer: { class: 'bg-white' },
@@ -55,7 +57,43 @@
                             <Icon name="x:calendar-1" class="text-[#92ACE5] w-11 h-11" />
                         </template>
                     </XDatePicker>
-                    <h3 class="text-blue font-normal text-[12px] m-0">mas criterios de busqueda </h3>
+
+                    <!-- Trigger -->
+                    <h3 class="text-blue font-normal text-[12px] m-0 cursor-pointer hover:underline flex items-center gap-1 mt-2"
+                        @click="showFilters = !showFilters">
+                        {{ showFilters ? 'Ocultar criterios de búsqueda' : 'Más criterios de búsqueda' }}
+                        <Icon :name="showFilters ? 'x:nav-arrow-down' : 'x:nav-arrow-up'" />
+                    </h3>
+
+                    <!-- Sección desplegable -->
+                    <transition name="fade">
+                        <div v-show="showFilters" class="mt-3">
+                            <!-- Subtítulo -->
+                            <h3 class="text-blue font-normal text-[12px] m-0 mb-2">
+                                Tipo de Transferencia
+                            </h3>
+
+                            <!-- Botones -->
+                            <div class="flex flex-wrap gap-2">
+                                <XButton label="QR" class="!w-[50px]" />
+                                <XButton label="Express" class="!w-[60px]" />
+                                <XButton label="Asíncrono" class="!w-[75px]" />
+                                <XButton label="Todos" class="!w-[50px]" />
+                            </div>
+                            <h3 class="text-blue font-normal text-[12px] m-0 mb-2">
+                                Moneda
+                            </h3>
+
+                            <!-- Botones -->
+                            <div class="flex flex-wrap gap-2">
+                                <XButton label="Gs" class="!w-[50px]" />
+                                <XButton label="BRL" class="!w-[50px]" />
+                                <XButton label="$US" class="!w-[50px]" />
+                                <XButton label="EUR" class="!w-[50px]" />
+                                <XButton label="Todos" class="!w-[50px]" />
+                            </div>
+                        </div>
+                    </transition>
                 </div>
                 <div
                     class="w-full sm:w-1/3 p-4 bg-white border border-gray-200 rounded-2xl h-65 flex flex-col justify-between">
@@ -63,38 +101,29 @@
                     <XInputText name="codigo" label="Numero de cuenta" placeholder="Ingresa numero" />
                 </div>
             </div>
-            <XSnackBar icon="x:info-empty" bg-circle="bg-[#EBF5FB]" closable class="border-[#86C1E9] mb-4"
-                class-icon="text-[#5F6A7B] w-[30px] h-[30px]">
+            <XSnackBar icon="x:info-empty" bg-circle="bg-[#A6C4F6]" closable class="border-[#A6C4F6] mb-4"
+                class-icon="text-[#3698DB] w-[22px] h-[22px]">
                 <p class="text-[#5F6A7B]">
                     Recuerda que al seleccionar la opción
                     <span class="font-bold">“Más criterios de búsqueda”</span>,
                     se habilitarán campos adicionales que te permitirán realizar la consulta de manera más específica.
                 </p>
             </XSnackBar>
-<div class="self-end flex gap-8">
-  <XButton 
-    label="Limpiar" 
-    :disabled="selectedBanks.length === 0"
-    variant="outlined"
-    class="!w-[150px]"
-    @click="handleChannelChange('channel')"
-  >
-    <template #icon>
-      <Icon name="x:clean" class="w-5 h-5" />
-    </template>
-  </XButton>
+            <div class="self-end flex gap-8">
+                <XButton label="Limpiar" :disabled="selectedBanks.length === 0" variant="outlined" class="!w-[150px]"
+                    @click="handleChannelChange('channel')">
+                    <template #icon>
+                        <Icon name="x:clean" class="w-5 h-5" />
+                    </template>
+                </XButton>
 
-  <XButton 
-    label="Buscar" 
-    :disabled="selectedBanks.length === 0"
-    class="!w-[200px]"
-    @click="handleChannelChange('contingency')"
-  >
-    <template #icon>
-      <Icon name="x:search" class="w-5 h-5" />
-    </template>
-  </XButton>
-</div>
+                <XButton label="Buscar" :disabled="selectedBanks.length === 0" class="!w-[200px]"
+                    @click="handleChannelChange('contingency')">
+                    <template #icon>
+                        <Icon name="x:search" class="w-5 h-5" />
+                    </template>
+                </XButton>
+            </div>
 
 
 
@@ -214,9 +243,9 @@
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue'
 const selectedBanks = ref<string[]>([]);
-
+const showFilters = ref(false);
 const accion1 = () => alert("Botón Enviados presionado");
 const accion2 = () => alert("Botón Recibidos presionado");
 const accion3 = () => alert("Botón Ambos presionado");
@@ -224,3 +253,16 @@ const accion3 = () => alert("Botón Ambos presionado");
 const handleChannelChange = (type: 'channel' | 'contingency') => {
 }
 </script>
+
+<style>
+/* Animación simple para desplegar */
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
